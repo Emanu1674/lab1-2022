9d26b29c8400aca000f177cb7276d86832429207
BuildTools <unconfigured@null.spigotmc.org>
2022-04-22 18:54:02 -0300

    Exer. Float

---
+++ Exercícios/A2 Implementação de números/E4 Float.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("O quilo de laranjas custa em média R$%.2f reais\n", (5.0 + 4.0 + 10.0)/3.0);
+    return 0;
+}
---
+++ Exercícios/A2 Implementação de números/E5 Float.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("A distância que o carro pode fazer com R$100 de gasolina é de %.2fkm\n", (100.0/7.0)*10.0);
+    return 0;
+}
